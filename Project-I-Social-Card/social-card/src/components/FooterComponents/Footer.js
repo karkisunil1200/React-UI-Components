2542026d1650260998fa66ce9ce893c323d36d63
Sunil Karki <karkisunil1200@gmail.com>
2019-05-25 15:51:38 -0500

    added footer part

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div class='footer'>
+      <i class='far fa-comment' />
+      <i class='far fas fa-sync' />
+      <i class='far fa-heart' />
+      <i class='far fas fa-envelope-square' />
+    </div>
+  );
+};
+export default Footer;
