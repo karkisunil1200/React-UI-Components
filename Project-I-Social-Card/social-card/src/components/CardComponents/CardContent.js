1b7dcbc4e06ab32dda64fc7b089940fed948ab30
Sunil Karki <karkisunil1200@gmail.com>
2019-05-25 15:05:46 -0500

    done with the header part

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div className='card-content'>
+      <p>React Makes it Painless to create interactive UIS. Design simple views for each state in your application.</p>
+    </div>
+  );
+};
